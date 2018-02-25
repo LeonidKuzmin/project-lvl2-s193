92423878aacca36be47442e87ea45e80c49ee76f
Leonid Kuzmin <leonid.kuzmin@mail.ru>
2018-02-25 10:27:29 +0500

    add support of recursive structure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,100 +1,186 @@
-import genDiff from '../src';
+import fs from 'fs';
+import genDiff, { genDiffWithoutRender } from '../src';
+
+const flatFilesExpectationAST1 = [
+  { action: ' ', key: 'host', value: 'hexlet.io' },
+  { action: '-', key: 'timeout', value: 50 },
+  { action: '+', key: 'timeout', value: 20 },
+  { action: '-', key: 'proxy', value: '123.234.53.22' },
+  { action: '+', key: 'verbose', value: true },
+];
+
+test('Flat files: Compare two JSON files without render', () => {
+  expect(genDiffWithoutRender('__tests__/__fixtures__/flat_before.json', '__tests__/__fixtures__/flat_after.json'))
+    .toEqual(flatFilesExpectationAST1);
+});
+
+
+const flatFilesExpectation = fs.readFileSync('__tests__/__fixtures__/flat_expectation.txt', 'utf8');
+
+test('Flat files: Compare two JSON files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.json', '__tests__/__fixtures__/flat_after.json'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare two YAML files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.yml', '__tests__/__fixtures__/flat_after.yml'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare JSON and YAML files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.json', '__tests__/__fixtures__/flat_after.yml'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare YAML and JSON files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.yml', '__tests__/__fixtures__/flat_after.json'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare two INI files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.ini', '__tests__/__fixtures__/flat_after.ini'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare JSON and INI files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.json', '__tests__/__fixtures__/flat_after.ini'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare INI and JSON files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.ini', '__tests__/__fixtures__/flat_after.json'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare YAML and INI files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.yml', '__tests__/__fixtures__/flat_after.ini'))
+    .toBe(flatFilesExpectation);
+});
+
+test('Flat files: Compare INI and YAML files', () => {
+  expect(genDiff('__tests__/__fixtures__/flat_before.ini', '__tests__/__fixtures__/flat_after.yml'))
+    .toBe(flatFilesExpectation);
+});
+
+
+const expectationAST1 = [
+  {
+    action: ' ',
+    key: 'common',
+    value: [
+      { action: ' ', key: 'setting1', value: 'Value 1' },
+      { action: '-', key: 'setting2', value: '200' },
+      { action: '-', key: 'setting3', value: true },
+      {
+        action: '+',
+        key: 'setting3',
+        value: [
+          { action: ' ', key: 'key', value: 'value' },
+        ],
+      },
+      {
+        action: ' ',
+        key: 'setting6',
+        value: [
+          { action: ' ', key: 'key', value: 'value' },
+          { action: '+', key: 'ops', value: 'vops' },
+        ],
+      },
+      { action: '+', key: 'setting4', value: 'blah blah' },
+      {
+        action: '+',
+        key: 'setting5',
+        value: [
+          { action: ' ', key: 'key5', value: 'value5' },
+        ],
+      },
+    ],
+  },
+
+  {
+    action: ' ',
+    key: 'group1',
+    value: [
+      { action: '-', key: 'baz', value: 'bas' },
+      { action: '+', key: 'baz', value: 'bars' },
+      { action: ' ', key: 'foo', value: 'bar' },
+      {
+        action: '-',
+        key: 'nest',
+        value: [
+          { action: ' ', key: 'key', value: 'value' },
+        ],
+      },
+      { action: '+', key: 'nest', value: 'str' },
+    ],
+  },
+
+  {
+    action: '-',
+    key: 'group2',
+    value: [
+      { action: ' ', key: 'abc', value: '12345' },
+    ],
+  },
+
+  {
+    action: '+',
+    key: 'group3',
+    value: [
+      { action: ' ', key: 'fee', value: '100500' },
+    ],
+  },
+];
+
+test('Compare two JSON files without render', () => {
+  expect(genDiffWithoutRender('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json'))
+    .toEqual(expectationAST1);
+});
+
+
+const expectation = fs.readFileSync('__tests__/__fixtures__/expectation.txt', 'utf8');
 
 test('Compare two JSON files', () => {
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare two YAML files', () => {
   expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare JSON and YAML files', () => {
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.yml'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare YAML and JSON files', () => {
   expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.json'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare two INI files', () => {
   expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare JSON and INI files', () => {
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.ini'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare INI and JSON files', () => {
   expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.json'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare YAML and INI files', () => {
   expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.ini'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
 
 test('Compare INI and YAML files', () => {
   expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.yml'))
-    .toBe(`{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
-}`);
+    .toBe(expectation);
 });
--- src/index.js
+++ src/index.js
@@ -16,27 +16,72 @@ const readFileToObj = (pathToFile) => {
   return parsers[format](content);
 };
 
-const compareObjects = (obj1, obj2) => {
-  const resultRows = _.union(Object.keys(obj1), Object.keys(obj2))
-    .reduce((acc, key) => {
-      if (!obj1[key]) {
-        return [...acc, `+ ${key}: ${obj2[key]}`];
-      }
-      if (!obj2[key]) {
-        return [...acc, `- ${key}: ${obj1[key]}`];
-      }
-      if (obj1[key] === obj2[key]) {
-        return [...acc, `  ${key}: ${obj1[key]}`];
-      }
-      return [...acc, `+ ${key}: ${obj2[key]}`, `- ${key}: ${obj1[key]}`];
-    }, [])
-    .map(row => `  ${row}`);
+const convertToAst = (subTree) => {
+  if (!_.isObject(subTree)) {
+    return subTree;
+  }
+  return Object.keys(subTree).reduce((acc, key) =>
+    [...acc, { action: ' ', key, value: convertToAst(subTree[key]) }], []);
+};
 
-  return ['{', ...resultRows, '}'].join('\n');
+const compareObjects = (obj1, obj2) => _.union(Object.keys(obj1), Object.keys(obj2))
+  .reduce((acc, key) => {
+    if (!obj1[key]) {
+      return [...acc, { action: '+', key, value: convertToAst(obj2[key]) }];
+    }
+    if (!obj2[key]) {
+      return [...acc, { action: '-', key, value: convertToAst(obj1[key]) }];
+    }
+    if (obj1[key] === obj2[key]) {
+      return [...acc, { action: ' ', key, value: convertToAst(obj1[key]) }];
+    }
+    if (!_.isObject(obj1[key]) || !_.isObject(obj2[key])) {
+      return [...acc,
+        { action: '-', key, value: convertToAst(obj1[key]) },
+        { action: '+', key, value: convertToAst(obj2[key]) },
+      ];
+    }
+    return [...acc, { action: ' ', key, value: compareObjects(obj1[key], obj2[key]) }];
+  }, []);
+/*
+const render = ast => ['{',
+  ...ast.map(({ action, key, value }) => {
+    if (_.isArray(value)) {
+      return `  ${action} ${key}: ${render(value)}`;
+    }
+    return `  ${action} ${key}: ${value}`;
+  }),
+  '}',
+  '',
+].join('\n');
+*/
+const render = (ast) => {
+  const helper = restAst => restAst
+    .reduce((acc, { action, key, value }) => {
+      if (!_.isArray(value)) {
+        return [...acc, `${action} ${key}: ${value}`];
+      }
+      return [...acc,
+        `${action} ${key}: {`,
+        ...helper(value).map(s => `  ${s}`),
+        '  }'];
+    }, [])
+    .map(s => `  ${s}`);
+
+  const rows = helper(ast).join('\n');
+
+  return `{\n${rows}\n}\n`;
+};
+
+export const genDiffWithoutRender = (pathToFile1, pathToFile2) => {
+  const obj1 = readFileToObj(pathToFile1);
+  const obj2 = readFileToObj(pathToFile2);
+  return compareObjects(obj1, obj2);
 };
 
 export default (pathToFile1, pathToFile2) => {
   const obj1 = readFileToObj(pathToFile1);
   const obj2 = readFileToObj(pathToFile2);
-  return compareObjects(obj1, obj2);
+  const ast = compareObjects(obj1, obj2);
+  return render(ast);
 };
